e8ae3b894f5d5eb158319f6d5672e2caa732faf6
RonnyU <ronnymagdiel@gmail.com>
2021-10-31 18:18:58 -0600

    Add new rules to gitIgnore

--- firebase/config.js
+++ firebase/config.js
@@ -1,12 +1,11 @@
-//Create a new project in google firebase and fill the info below with your info
 const firebaseConfig = {
-  apiKey: 'Put your API key here',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyCX2zNcDjKcXXfK7iFmq1Yi81QTcDrkiMg',
+  authDomain: 'business-hunt-8e895.firebaseapp.com',
+  databaseURL: 'gs://business-hunt-8e895.appspot.com',
+  projectId: 'business-hunt-8e895',
+  storageBucket: 'business-hunt-8e895.appspot.com',
+  messagingSenderId: '922564686306',
+  appId: '1:922564686306:web:30d0c05d5040981f7049bb',
 };
 
 export default firebaseConfig;
